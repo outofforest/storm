863b6f70b313746d0721f15f51b5e38a27174ba5
Wojciech Malota-Wojcik <wojtek@exw.co>
2023-08-18 23:07:14 +0200

    fix

--- cache/types_test.go
+++ cache/types_test.go
@@ -7,10 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const alignment = 8
+
 func TestHeaderSize(t *testing.T) {
 	assert.LessOrEqual(t, uint64(unsafe.Sizeof(Header{})), uint64(CacheHeaderSize))
 }
 
 func TestHeaderSizeIsAligned(t *testing.T) {
-	assert.Equal(t, 0, CacheHeaderSize%8)
+	assert.Equal(t, 0, CacheHeaderSize%alignment)
 }
