887f328a2266c6b05649a0f9d5eace1687beda87
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-08-22 20:18:01 +0200

    Introduce blob block type (#28)

--- blocks/types_test.go
+++ blocks/types_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/outofforest/storm/blocks"
+	blobV0 "github.com/outofforest/storm/blocks/blob/v0"
 	objectlistV0 "github.com/outofforest/storm/blocks/objectlist/v0"
 	pointerV0 "github.com/outofforest/storm/blocks/pointer/v0"
 	singularityV0 "github.com/outofforest/storm/blocks/singularity/v0"
@@ -16,6 +17,7 @@ func TestBlockSizes(t *testing.T) {
 	assertDiskSize[singularityV0.Block](t)
 	assertDiskSize[pointerV0.Block](t)
 	assertDiskSize[objectlistV0.Block](t)
+	assertDiskSize[blobV0.Block[[blocks.BlockSize]byte]](t)
 }
 
 func assertDiskSize[T blocks.Block](t *testing.T) {
