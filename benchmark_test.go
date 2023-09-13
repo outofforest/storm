bd757ea432359aa4e5644e4ee77371c922783867
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-09-13 18:30:46 +0200

    Benchmark of `storm` (#76)

---
+++ benchmark_test.go
@@ -0,0 +1,67 @@
+package storm
+
+import (
+	"crypto/rand"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/outofforest/storm/blocks"
+	"github.com/outofforest/storm/cache"
+	"github.com/outofforest/storm/persistence"
+	"github.com/outofforest/storm/pkg/filedev"
+)
+
+// go test -bench=. -run=^$ -cpuprofile profile.out -benchtime=5x
+// go tool pprof -http="localhost:8000" pprofbin ./profile.out
+
+func BenchmarkStorm(b *testing.B) {
+	const size = 30000
+
+	b.StopTimer()
+	b.ResetTimer()
+
+	requireT := require.New(b)
+
+	keys := make([][48]byte, size)
+
+	for i := 0; i < len(keys); i++ {
+		_, err := rand.Read(keys[i][:])
+		requireT.NoError(err)
+	}
+
+	f, err := os.OpenFile("/home/wojciech/testdev", os.O_RDWR, 0o600)
+	requireT.NoError(err)
+	defer f.Close()
+
+	// dev := memdev.New(300 * 1024 * 1024)
+	dev := filedev.New(f)
+	for bi := 0; bi < b.N; bi++ {
+		requireT.NoError(persistence.Initialize(dev, true))
+
+		s, err := persistence.OpenStore(dev)
+		requireT.NoError(err)
+
+		c, err := cache.New(s, 500*1024*1024)
+		requireT.NoError(err)
+
+		storm := New[item](c, blocks.SpaceID(0), 100)
+
+		b.StartTimer()
+		func() {
+			for i := 0; i < len(keys); i++ {
+				_ = storm.Set(keys[i][:], item{Field: uint64(i)})
+			}
+
+			_ = c.Commit()
+		}()
+
+		func() {
+			for i := 0; i < len(keys); i++ {
+				_, _, _ = storm.Get(keys[i][:])
+			}
+		}()
+		b.StopTimer()
+	}
+}
